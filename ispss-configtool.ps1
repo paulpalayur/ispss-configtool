21f2be3b6c417644e8fff91c53da1311d8c801cd
Brad McDowell <65890052+bradmcdowell@users.noreply.github.com>
2022-09-28 17:16:33 +1000

    New menu (#2)
    * Changed the menu
    
    * Fixed Menu
    
    * New Menu Changes

--- ispss-configtool.ps1
+++ ispss-configtool.ps1
@@ -1,22 +1,24 @@
-# CyberArk Privilege Cloud Service Menu
-$version = "21.07.09"
-# List all Cyberark Connector/PSM Servers here
-$PSMServers = "connector1","connector2"
-$CPMServers = "connector1"
-$SecureTunnelServers = "connector1","connector2"
+# CyberArk Privilege ISPSS Config Tool
+$version = "22.09.28"
+# Prompt user for ISPSS URL
+$ISPSSURL = Read-Host -Prompt 'Input your ISPSSURL eg https://subdomain.cyberark.cloud'
+
 ###########################################
 
 function Show-Menu {
     param (
-        [string]$Title = 'CyberArk Privilege Cloud Service Menu - Version'
+        [string]$Title = 'CyberArk ISPSS Config Tool - Version'
     )
     Clear-Host
     Write-Host "================ $Title $version ================"
     
-    Write-Host "1: Press '1' for PSM service restart on all PSM Servers."
-    Write-Host "2: Press '2' for CPM service restart on all CPM Servers."
-    Write-Host "3: Press '3' for Secure Tunnel service restart on all Secure Tunnel Servers."
-    Write-Host "4: Press '4' for list of script configuration."
+    Write-Host "1: Press '1' Authenticate to ISPSS"
+    Write-Host "2: Press '2' Create Identity Auth Profiles"
+    Write-Host "3: Press '3' Create Identity Policies"
+    Write-Host "4: Press '4' Pre-Requisties Checklist"
+    Write-Host "5: Press '5' Install PSM Health Check"
+    Write-Host "6: Press '6' Run PSMConfigureAppLocker Script"
+    Write-Host "7: Press '7' Run Move PSMConnect and PSMAdminConnect Script"
     Write-Host "Q: Press 'Q' to quit."
 }
 
@@ -27,51 +29,41 @@ do
     switch ($selection)
     {
     '1' {
-    Write-Host 'You chose option #1 - PSM service restart on all PSM Servers.'
-    # Cyber-Ark Privileged Session Manager
-    Foreach ($PSMserver in $PSMServers)
-    {
-    Write-Host "Attempting Cyber-Ark Privileged Session Manager service restart for $PSMserver"
-    get-service 'Cyber-Ark Privileged Session Manager' -ComputerName $PSMserver | select Status,DisplayName
-    get-service 'Cyber-Ark Privileged Session Manager' -ComputerName $PSMserver | Restart-Service
-    get-service 'Cyber-Ark Privileged Session Manager' -ComputerName $PSMserver | select Status,DisplayName
-    Write-Host "Completed Cyber-Ark Privileged Session Manager service restart for $PSMserver" -ForegroundColor Green
-    }
+    # Start Option 1        
+    Write-Host 'You chose option #1 - Authenticate to ISPSS'
+    Write-host $ISPSSURL
+    # End Option 1
     ###########################################
     } '2' {
-    Write-Host 'You chose option #2 - CPM service restart on all CPM Servers.'
-    # CyberArk Password Manager
-    Foreach ($CPMserver in $CPMServers)
-    {
-    Write-Host "Attempting CyberArk Password Manager service restart for $CPMserver"
-    get-service 'CyberArk Password Manager' -ComputerName $CPMserver | select Status,DisplayName
-    get-service 'CyberArk Password Manager' -ComputerName $CPMserver | Restart-Service
-    get-service 'CyberArk Password Manager' -ComputerName $CPMserver | select Status,DisplayName
-    Write-Host "Completed CyberArk Password Manager service restart for $CPMserver" -ForegroundColor Green
-    }
+    # Start Option 2        
+    Write-Host 'You chose option #2 - Create Identity Auth Profiles'
+    # End Option 2
     ###########################################
-
     } '3' {
-    Write-Host 'You chose option #3 - Secure Tunnel service restart on all Secure Tunnel Servers.'
-    # CyberArkPrivilegeCloudSecureTunnel
-    Foreach ($TunnelServer in $SecureTunnelServers)
-    {
-    Write-Host "Attempting CyberArkPrivilegeCloudSecureTunnel service restart for $TunnelServer"
-    get-service CyberArkPrivilegeCloudSecureTunnel -ComputerName $TunnelServer | select Status,DisplayName
-    get-service CyberArkPrivilegeCloudSecureTunnel -ComputerName $TunnelServer | Restart-Service
-    get-service CyberArkPrivilegeCloudSecureTunnel -ComputerName $TunnelServer | select Status,DisplayName
-    Write-Host "Completed CyberArkPrivilegeCloudSecureTunnel service restart for $TunnelServer" -ForegroundColor Green
-    }
+    # Start Option 3        
+    Write-Host 'You chose option #3 - Create Identity Policies'
+    # End Option 3
     ###########################################
     } '4' {
-    Write-Host 'You chose option #4 - list of script configuration.'
-    Write-Host "Array of PSM Servers"
-    Write-Host $PSMServers -ForegroundColor Green
-    Write-Host "Array of CPM Servers"
-    Write-Host $CPMServers -ForegroundColor Green
-    Write-Host "Array of Secure Tunnel Servers"
-    Write-Host $SecureTunnelServers -ForegroundColor Green
-    Write-Host "If you want the servers changes, please update lines 2-4 of this script."
+    # Start Option 3        
+    Write-Host 'You chose option #4 - Pre-Requisties Checklist'
+    # End Option 3
+    ###########################################    
+    } '5' {
+    # Start Option 3        
+    Write-Host 'You chose option #5 - Install PSM Health Check'
+    # End Option 3
+    ###########################################   
+    } '6' {
+    # Start Option 3        
+    Write-Host 'You chose option #6 - Run PSMConfigureAppLocker Script'
+    # End Option 3
+    ###########################################   
+    } '7' {
+    # Start Option 3        
+    Write-Host 'You chose option #7 - Run Move PSMConnect and PSMAdminConnect Script'
+    # End Option 3
+    ###########################################
     }
     }
     pause
